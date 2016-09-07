010998830fdd491c4911c87c432c0a61f243893d
Sergey <klikkn@yandex.ru>
2016-09-07 23:33:55 +0500

    Update

---
+++ src/theme/footer.php
@@ -0,0 +1,11 @@
+<div class="footer">
+	
+</div>
+
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/vendors.js"></script>
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/main.js"></script>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ src/theme/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/img/favicon.ico" type="image/png">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/vendors.css" />
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/main.css" />
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<div class="header">
+	
+</div>
+
