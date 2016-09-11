6b43cbce6e85e2bd856a5abdb38126eac53c0abb
Sergey <klikkn@yandex.ru>
2016-09-11 22:49:55 +0500

    add index

--- src/theme/index.php
+++ src/theme/index.php
@@ -1,10 +1,19 @@
 <?php get_header() ?>
 
+	<?php
+		$page_data = get_page(10);
+		$title = apply_filters('the_title', $page_data->post_title);
+		$content = apply_filters('the_content', $page_data->post_content);
+	?>
 
-<div class="content">
-	<div class="content__title">
-		
+	<h1 class="title index__title">
+		<?php echo $title; ?>
+	</h1>
+
+	<div class="content index__content">
+		<?php echo $content; ?>
 	</div>
-</div>
+
+
 
 <?php get_footer() ?>
