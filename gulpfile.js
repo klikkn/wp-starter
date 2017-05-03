240609ef982276e92b164ba365f7fc9ff1f202f4
Sergey <klikkn@yandex.ru>
2017-05-03 09:40:36 +0500

    remove unused comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,10 @@
 var gulp = require('gulp')
 var tasks = require('./gulp/')
 
-/* @gulp: default */
 gulp.task('default', ['bower', 'assets', 'js', 'styles', 'php', 'watch'], tasks.BSync)
-
-/* @gulp: javascript */
 gulp.task('js', tasks.Javascript)
-
-/* @gulp: bower */
 gulp.task('bower', tasks.Bower)
-
-/* @gulp: assets */
 gulp.task('assets', tasks.Assets)
-
-/* @gulp: dist */
 gulp.task('php', tasks.Php)
-
-/* @gulp: styles */
 gulp.task('styles', tasks.Styles)
-
-/* @gulp: watch */
 gulp.task('watch', tasks.Watch)
